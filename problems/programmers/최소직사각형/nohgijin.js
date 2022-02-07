88c4c7ef563bf414d5b1d368b5219abc5bdc7aea
nohgijin <ynda11111@naver.com>
2022-02-07 10:58:52 +0900

    220207

--- problems/programmers/최소직사각형/nohgijin.js
+++ problems/programmers/최소직사각형/nohgijin.js
@@ -1,21 +1,14 @@
 function solution(sizes) {
   var answer = 0;
-  const parsingSize = [];
+  const large = [];
+  const small = [];
 
   for (let i = 0; i < sizes.length; i++) {
-    const size = sizes[i];
-
-    if (size[0] > size[1]) {
-      parsingSize[i] = [size[1], size[0]];
-    } else {
-      parsingSize[i] = size;
-    }
+    large.push(Math.max(sizes[i][0], sizes[i][1]));
+    small.push(Math.min(sizes[i][0], sizes[i][1]));
   }
 
-  const leftSizes = parsingSize.map((size) => size[0]).sort((a, b) => a - b);
-  const rightSizes = parsingSize.map((size) => size[1]).sort((a, b) => a - b);
-
-  answer = leftSizes[leftSizes.length - 1] * rightSizes[rightSizes.length - 1];
+  answer = Math.max(...large) * Math.max(...small);
 
   return answer;
 }
